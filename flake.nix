823151cbd4018109a9cd74e06f99828c76f35caf
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-08-09 23:30:34 +0700

    chore: init flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Highcall EPK Development Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        # node
+        nodejs
+        eslint_d
+        prettierd
+        tailwindcss-language-server
+        vscode-langservers-extracted
+
+        # utils
+        jq
+        bruno
+        lazygit
+        posting
+      ];
+
+      shellHook = ''
+        echo "Highcall EPK development shell activated."
+      '';
+    };
+  };
+}
