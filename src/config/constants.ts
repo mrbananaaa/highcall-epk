8e9985cfab0fa1b20a6e6151ee40478ebf05b194
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-09-10 20:00:52 +0700

    feat: map new images

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -11,11 +11,10 @@ export const CONTACT_LIST = {
 export const IMGS = {
   kopiMantu: [
     "at-kopi-mantu-1.jpeg",
-    "at-kopi-mantu-1.jpeg",
-    "at-kopi-mantu-1.jpeg",
-    "at-kopi-mantu-1.jpeg",
-    "at-kopi-mantu-1.jpeg",
-    "at-kopi-mantu-1.jpeg",
+    "at-kopi-mantu-2.jpeg",
+    "at-kopi-mantu-3.jpeg",
+    "at-kopi-mantu-4.jpeg",
+    "at-kopi-mantu-5.jpeg",
   ],
   dopamine: [],
 } as const;
